1859df6f0d18de799a6934547db7e562f0c5b627
mastiff-systems343 <lai@mastiffsystems.com>
2025-02-07 17:23:43 -0600

    Ensure deploy script and Procfile are correct

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
-# deploy.sh
-bundle install
-bundle exec jekyll serve --host 0.0.0.0 --port $PORT
+#!/bin/bash
+echo "Starting Deployment Script"
+bundle install --verbose || exit 1
+bundle exec jekyll serve --host 0.0.0.0 --port $PORT --trace || exit 1
